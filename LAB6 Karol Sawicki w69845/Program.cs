d407cc29d7a174f451a96c6725254f81d1268103
WildGenie7 <148540149+WildGenie7@users.noreply.github.com>
2025-02-10 12:09:28 +0100

    LAB6

---
+++ LAB6 Karol Sawicki w69845/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj numer albumu: ");
+        string numerAlbumu = Console.ReadLine();
+
+        Console.WriteLine("Podaj nazwę pliku: ");
+        string nazwaPliku = Console.ReadLine();
+        
+        string json = JsonConvert.SerializeObject(numerAlbumu, Formatting.Indented);
+
+        File.WriteAllText(nazwaPliku, json);
+
+        Console.WriteLine($"Dane zapisano w pliku {nazwaPliku}.");
+
+/////////////////     ZAD2                  ///////////////////////////////////////////////
+
+        Console.WriteLine("Podaj nazwę pliku do odczytu: ");
+        string nazwaPlik = Console.ReadLine();
+
+        try
+        {
+            string zawartosc = File.ReadAllText(nazwaPlik);
+
+            Console.WriteLine($"Zawartość pliku {nazwaPlik}:");
+            Console.WriteLine(zawartosc);
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine($"Błąd: Plik o nazwie {nazwaPlik} nie istnieje.");
+        }
+
+        /////////////////////////// ZAD3 ///////////////////////////////////////////////////////////////////
+        string nazwaPeseli = "pesele.txt";
+
+        try
+        {
+            List<string> pesels = new List<string>(File.ReadAllLines(nazwaPeseli));
+
+            Console.WriteLine($"Wczytano {pesels.Count} peseli z pliku {nazwaPeseli}.");
+
+            int liczbaZenskichPeseli = PoliczZenskiePesels(pesels);
+
+            Console.WriteLine($"Liczba żeńskich peseli: {liczbaZenskichPeseli}");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine($"Błąd: Plik {nazwaPeseli} nie istnieje.");
+        }
+    }
+
+    static int PoliczZenskiePesels(List<string> pesels)
+    {
+        int liczbaZenskich = 0;
+
+        foreach (string pesel in pesels)
+        {
+            if (pesel.Length == 11 && (pesel[9] - '0') % 2 == 0) 
+            {
+                liczbaZenskich++;
+            }
+        }
+
+        return liczbaZenskich;
+    }
+}
+
