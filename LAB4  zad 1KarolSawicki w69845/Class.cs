b65a7efcb7e8b6ab8fe674e95e3da351357b34b2
WildGenie7 <148540149+WildGenie7@users.noreply.github.com>
2025-02-12 09:40:19 +0100

    Add files via upload

---
+++ LAB4  zad 1KarolSawicki w69845/Class.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB4_KarolSawicki_w69845
+{
+    public class Shape
+    {
+        public decimal X { get; set; }
+        public decimal Y { get; set; }
+        public decimal Height { get; set; }
+        public decimal Width { get; set; }
+
+        public Shape(decimal X, decimal Y, decimal Height, decimal Width)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Height = Height;
+            this.Width = Width;
+        }
+
+        public virtual void Draw()
+        {
+            Console.WriteLine("Rysuję figurę");
+        }
+
+        public override string ToString()
+        {
+            return $"Figura o współrzędnych ({X}, {Y}) i wymiarach {Width} na {Height}.";
+        }
+
+        public class Rectangle : Shape
+        {
+            public Rectangle(decimal X, decimal Y, decimal Height, decimal Width) : base(X, Y, Height, Width) { }
+
+            public override void Draw()
+            {
+                Console.WriteLine("Rysuję Rectangle!");
+            }
+        }
+
+        public class Triangle : Shape
+        {
+            public Triangle(decimal X, decimal Y, decimal Height, decimal Width) : base(X, Y, Height, Width) { }
+
+            public override void Draw()
+            {
+                Console.WriteLine("Rysuję Triangle!");
+            }
+        }
+
+        public class Cirlce : Shape
+        {
+            public Cirlce(decimal X, decimal Y, decimal Height, decimal Width) : base(X, Y, Height, Width) { }
+
+            public override void Draw()
+            {
+                Console.WriteLine("Rysuję Circle!");
+            }
+        }
+
+    }
+}
---
+++ LAB4  zad 1KarolSawicki w69845/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.CompilerServices;
+using System;
+using System.Collections.Generic;
+using LAB4_KarolSawicki_w69845;
+using static LAB4_KarolSawicki_w69845.Shape;
+
+
+namespace LAB4_KarolSawicki_w69845
+{ 
+
+    class program
+    { 
+        static void Main(string[] args) 
+        {
+            List<Shape> figury = new List<Shape>();
+            Shape kolo = new Cirlce(12, 22, 80, 12);
+            kolo.Draw();
+            figury.Add(kolo);
+
+            Shape trojkat = new Triangle(1, 2, 3, 4);
+            trojkat.Draw();
+            figury.Add(trojkat);
+
+            Shape prostokat = new Rectangle(56, 23, 43, 90);
+            prostokat.Draw();
+            figury.Add(prostokat);
+
+            Console.WriteLine("Sprawdzenie zawartości listy:\n");
+
+            foreach (Shape shape in figury)
+            {
+            Console.WriteLine(shape);
+            }
+
+
+        }
+    }
+}
+
+    
